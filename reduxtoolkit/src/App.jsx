8a1978f6f823f2c5f190be9038f066f54f002a2c
phaneendra73 <phaneendra3377@gmail.com>
2024-03-28 01:35:26 +0530

    started the redux tool kit

---
+++ reduxtoolkit/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Learn redux tool kit</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ reduxtoolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialsate = {
+  id: 1,
+  description: "hello world",
+  status: false,
+};
