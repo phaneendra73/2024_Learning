38471e2685d2005fb80feffff6dd26044fafcf61
Turbobot <turbobot@vercel.com>
2024-08-02 21:58:51 +0530

    feat(create-turbo): apply official-starter transform

--- tswithhitesh/interface.ts
+++ tswithhitesh/interface.ts
@@ -1,5 +1,16 @@
 type user = {
   email: string;
-  name: string;
-  stringname(): string;
+  password: string;
+  phonenumber: string;
 };
+
+type user2 = Pick<user, 'email' | 'password'>;
+
+type partialuser2 = Partial<user2>;
+
+// PICK
+// PARTIAL
+// READONLY GENERICS
+// RECORD OR MAP
+// EXCLUDE
+// ZOD INFER TYPES
